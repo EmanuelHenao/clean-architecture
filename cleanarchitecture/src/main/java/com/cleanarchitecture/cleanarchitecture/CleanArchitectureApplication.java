c9d5b5e1a95e715fb91608bb2cb920c0f39fe5be
EmanuelHenao <emanuelhenaogiraldo@gmail.com>
2024-07-30 18:59:25 -0500

    disable temp database config

--- cleanarchitecture/src/main/java/com/cleanarchitecture/cleanarchitecture/CleanArchitectureApplication.java
+++ cleanarchitecture/src/main/java/com/cleanarchitecture/cleanarchitecture/CleanArchitectureApplication.java
@@ -2,8 +2,10 @@ package com.cleanarchitecture.cleanarchitecture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//disable temp database configuration
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CleanArchitectureApplication {
 
 	public static void main(String[] args) {
