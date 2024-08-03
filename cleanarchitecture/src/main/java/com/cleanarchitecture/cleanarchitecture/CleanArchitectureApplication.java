6a29b9e44761b866143bf330416a543b1bcfe0bd
EmanuelHenao <emanuelhenaogiraldo@gmail.com>
2024-08-03 15:00:39 -0500

    update main

--- cleanarchitecture/src/main/java/com/cleanarchitecture/cleanarchitecture/CleanArchitectureApplication.java
+++ cleanarchitecture/src/main/java/com/cleanarchitecture/cleanarchitecture/CleanArchitectureApplication.java
@@ -3,10 +3,14 @@ package com.cleanarchitecture.cleanarchitecture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 //disable temp database configuration
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+        SecurityAutoConfiguration.class
+//        ManagementWebSecurityAutoConfiguration.class
+})
 @ComponentScan(basePackages = {"com.cleanarchitecture.cleanarchitecture"})
 public class CleanArchitectureApplication {
 
